070a77097e3e3e39f029afa250fd2fce4b61a761
Ant-Lopez <anthony.lopez@techhigh.us>
2014-09-18 15:04:46 -0400

    Decomposition  Inserted

--- DrawFigures1.java
+++ DrawFigures1.java
@@ -1,56 +1,57 @@
 public class DrawFigures1 {
     public static void main(String[] args) {
-        System.out.println("   /\\");
-        System.out.println("  /  \\");
-        System.out.println(" /    \\");
-        System.out.println(" \\    /");
-        System.out.println("  \\  /");
-        System.out.println("   \\/");
-        System.out.println();
-        System.out.println(" \\    /");
-        System.out.println("  \\  /");
-        System.out.println("   \\/");
-        System.out.println("   /\\");
-        System.out.println("  /  \\");
-        System.out.println(" /    \\");
-        System.out.println();
-        System.out.println("   /\\");
-        System.out.println("  /  \\");
-        System.out.println(" /    \\");
-        System.out.println("+------+");
-        System.out.println("|      |");
-        System.out.println("|      |");
-        System.out.println("+------+");
-        System.out.println("|United|");
-        System.out.println("|States|");
-        System.out.println("+------+");
-        System.out.println("|      |");
-        System.out.println("|      |");
-        System.out.println("+------+");
-        System.out.println("   /\\");
-        System.out.println("  /  \\");
-        System.out.println(" /    \\");
-       
-        public static void Draw1() {
-9       System.out.println("   /\\");
-        System.out.println("  /  \\");
-        System.out.println(" /    \\");
-        System.out.println("+------+");
-        System.out.println("|      |");
-        System.out.println("|      |");
-        System.out.println("+------+");
-        System.out.println("|United|");
-        System.out.println("|States|");
-        System.out.println("+------+");
-        System.out.println("|      |");
-        System.out.println("|      |");
-        System.out.println("+------+")
-
-
-
-
-10 }
+        Diamon();
         
+        System.out.println();
+        BigX();
+        System.out.println();
+        Rocket();
+        Cone();
         
     }
-}
+       
+     public static void Rocket() {
+        System.out.println("   /\\");
+        System.out.println("  /  \\");
+        System.out.println(" /    \\");
+        System.out.println("+------+");
+        System.out.println("|      |");
+        System.out.println("|      |");
+        System.out.println("+------+");
+        System.out.println("|United|");
+        System.out.println("|States|");
+        System.out.println("+------+");
+        System.out.println("|      |");
+        System.out.println("|      |");
+        System.out.println("+------+");
+ }
+        
+     public static void Diamon() {
+        System.out.println("   /\\");
+        System.out.println("  /  \\");
+        System.out.println(" /    \\");
+        System.out.println(" \\    /");
+        System.out.println("  \\  /");
+        System.out.println("   \\/");
+
+ }    
+     
+      public static void BigX() {
+        System.out.println(" \\    /");
+        System.out.println("  \\  /");
+        System.out.println("   \\/");
+        System.out.println("   /\\");
+        System.out.println("  /  \\");
+        System.out.println(" /    \\");
+
+ }    
+      
+       public static void Cone() {
+      
+        System.out.println("   /\\");
+        System.out.println("  /  \\");
+        System.out.println(" /    \\");
+
+ }    
+    }
+
