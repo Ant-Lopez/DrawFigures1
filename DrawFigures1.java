ca61ca2b19e2a41b8deebee9f8e1e2069e0e4d12
Ant-Lopez <anthony.lopez@techhigh.us>
2014-09-19 15:23:21 -0400

    Update DrawFigures1.java

--- DrawFigures1.java
+++ DrawFigures1.java
@@ -1,7 +1,8 @@
 public class DrawFigures1 {
-    public static void main(String[] args) {
+    public static void main(String[] args) {//This is the Main Method
+    //This code calls all the other Methods
         Diamon();
-        
+        //The System.out.println statements are used for spaces.
         System.out.println();
         BigX();
         System.out.println();
@@ -9,7 +10,7 @@ public class DrawFigures1 {
         Cone();
         
     }
-       
+           //This method is called Third
      public static void Rocket() {
         System.out.println("   /\\");
         System.out.println("  /  \\");
@@ -25,7 +26,7 @@ public class DrawFigures1 {
         System.out.println("|      |");
         System.out.println("+------+");
  }
-        
+            //This method is called First
      public static void Diamon() {
         System.out.println("   /\\");
         System.out.println("  /  \\");
@@ -35,7 +36,7 @@ public class DrawFigures1 {
         System.out.println("   \\/");
 
  }    
-     
+        //This method is called Second 
       public static void BigX() {
         System.out.println(" \\    /");
         System.out.println("  \\  /");
@@ -45,7 +46,7 @@ public class DrawFigures1 {
         System.out.println(" /    \\");
 
  }    
-      
+      //This method is called last
        public static void Cone() {
       
         System.out.println("   /\\");
